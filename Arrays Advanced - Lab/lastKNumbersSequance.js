ab71f03b3617e63570b69fa6174d87d015d08a34
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-11 16:59:35 +0200

    Create lastKNumbersSequance.js

---
+++ Arrays Advanced - Lab/lastKNumbersSequance.js
@@ -0,0 +1,28 @@
+function lastKNumbersSequance(n, k) {
+    function sumKElements(k, arr, i) {
+
+        let sum = 0;
+
+        for (let index = i - 1; index >= 0; index--) {
+
+            if (index < i - k || index < 0) break;
+
+            sum += arr[index];
+
+        }
+
+        return sum;
+    }
+    let arr = [];
+
+    arr.push(1);
+
+    for (let index = 1; index < n; index++) {
+
+        arr.push(sumKElements(k, arr, index));
+
+    }
+    console.log(arr.join(" "));
+}
+lastKNumbersSequance(6, 3);
+lastKNumbersSequance(8, 2);
