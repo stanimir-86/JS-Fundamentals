c7d4ee6f02f05e4610b812a1071a32fc06a7e927
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-12 13:41:35 +0200

    Create sortAnArrayBy2Criteria.js

---
+++ Arrays Advanced-Exercise/sortAnArrayBy2Criteria.js
@@ -0,0 +1,13 @@
+function sortAnArrayBy2Criteria(input) {
+    input.sort((a, b) => {
+        if (a.length - b.length === 0) {
+            return a.localeCompare(b);
+        }
+        return a.length - b.length;
+    });
+    console.log(input.join("\n"));
+}
+sortAnArrayBy2Criteria(['alpha',
+    'beta',
+    'gamma']
+);
