20c62fd01e81299c27ccd40c46c6a49d66edbb94
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-14 19:42:30 +0200

    Create set.js

---
+++ Associative Arrays - Lab/set.js
@@ -0,0 +1,8 @@
+let set = new Set();//същия метод като map,но държи само ключове.
+//ако добавя повторяем ключ,игнорира го.
+//има същите методи като мап
+set.add("key");
+set.add("pes");
+set.add("key");
+set.add("keydog");
+console.log(set);
