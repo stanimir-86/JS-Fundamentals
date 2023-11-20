4ef5fff647d9608749cda1195a653e3787e51a2d
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-20 22:38:56 +0200

    Create convertMetersToKilometres.js

---
+++ Data Types and Variables - Exercise/convertMetersToKilometres.js
@@ -0,0 +1,9 @@
+function convertMetersToKilometres(distance) {
+
+    let meters = 1000;
+    let kilometers = distance / meters;
+    console.log(kilometers.toFixed(2));
+
+}
+convertMetersToKilometres(1852);
+convertMetersToKilometres(798);
