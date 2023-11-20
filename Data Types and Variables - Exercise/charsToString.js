43ed175c336274608d7e3cb036580f37c7044f36
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-20 22:38:54 +0200

    Create charsToString.js

---
+++ Data Types and Variables - Exercise/charsToString.js
@@ -0,0 +1,14 @@
+function charsToString(parm1, parm2, parm3) {
+
+    console.log(`${parm1}${parm2}${parm3}`);
+
+}
+charsToString('a',
+    'b',
+    'c');
+charsToString('%',
+    '2',
+    'o');
+charsToString('1',
+    '5',
+    'p');
