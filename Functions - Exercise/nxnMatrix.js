4f60ea625bbb7d941f13c48b533e3de58e27260a
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-11-27 21:47:05 +0200

    Create nxnMatrix.js

---
+++ Functions - Exercise/nxnMatrix.js
@@ -0,0 +1,14 @@
+function nxnMatrix(num) {
+    for (let i = 0; i < num; i++) {
+        let rowElement = "";
+        for (let j = 0; j < num; j++) {
+            rowElement += num + " ";
+
+        }
+        console.log(rowElement);
+    }
+    
+}
+nxnMatrix(3);
+nxnMatrix(7);
+nxnMatrix(2);
