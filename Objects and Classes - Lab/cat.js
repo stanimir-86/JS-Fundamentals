cc4fedd61c1247cee346d0e988de6b37f7cdd685
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-12-01 01:01:05 +0200

    Create cat.js

---
+++ Objects and Classes - Lab/cat.js
@@ -0,0 +1,18 @@
+function cat(input) {
+
+    class Cat {
+        constructor(name, age) {
+            this.name = name,
+                this.age = age
+        }
+        meow() {
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+    for (let catInfo of input) {
+        let [name, age] = catInfo.split(" ");
+        let newCat = new Cat(name, age);
+        newCat.meow();
+    }
+}
+cat(['Mellow 2', 'Tom 5']);
