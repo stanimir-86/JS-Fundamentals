65c045f521aabfb26cfa8d9cf7ffb6595bdf088e
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2023-12-04 21:10:12 +0200

    Create stringSubstring.js

---
+++ Text Processing - Exercise/stringSubstring.js
@@ -0,0 +1,18 @@
+function stringSubstring(word, text) {
+
+    let textArray = text.split(" ");
+    let isNotFound = true;
+
+    for (const element of textArray) {
+        if (word.toLowerCase() === element.toLowerCase()) {
+            console.log(word);
+            isNotFound = false;
+            break;
+        }
+    }
+    if (isNotFound) {
+        console.log(`${word} not found!`);
+    }
+}
+stringSubstring('javascript',
+    'JavaScript is the best programming language');
