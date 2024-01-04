84c6f281bf1eff4f896cfd549332872bc8cc4462
stanimir-86 <121422047+stanimir-86@users.noreply.github.com>
2024-01-04 23:34:31 +0200

    Create sortNumbers.js

---
+++ MoreExercisesBasicSyntax/sortNumbers.js
@@ -0,0 +1,15 @@
+function sortNumbers(num1, num2, num3) {
+
+    let counter = 0;
+
+    while (counter < 3) {
+
+
+        
+    }
+
+
+}
+sortNumbers(2, 1, 3);
+// sortNumbers(-2, 1, 3);
+// sortNumbers(0, 0, 2);
